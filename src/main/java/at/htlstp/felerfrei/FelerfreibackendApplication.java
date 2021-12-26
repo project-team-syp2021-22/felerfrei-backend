1f3a89ab2c379cdbf0e53f91cd44a61940c1b80d
Florian Kainrath <flo.kainrath@gmail.com>
2021-12-26 14:53:06 +0100

    init

---
+++ src/main/java/at/htlstp/felerfrei/FelerfreibackendApplication.java
@@ -0,0 +1,13 @@
+package at.htlstp.felerfrei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelerfreibackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FelerfreibackendApplication.class, args);
+    }
+
+}
